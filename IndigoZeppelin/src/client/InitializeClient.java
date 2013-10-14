9fc365729a9fd126ed95c16cb45109a4e84f6969
qqsmalleindigoqq <wa_waxd@msn.com>
2013-10-14 18:56:13 +0200

    dpdpdp

--- IndigoZeppelin/src/client/InitializeClient.java
+++ IndigoZeppelin/src/client/InitializeClient.java
@@ -16,7 +16,7 @@ public class InitializeClient {
 	
 	public static void main(String[] args)
 	{
-		final GUIMain gui = new GUIMain();
+		
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -39,7 +39,7 @@ public class InitializeClient {
             java.util.logging.Logger.getLogger(GUIMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
+        final GUIMain gui = new GUIMain();
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
