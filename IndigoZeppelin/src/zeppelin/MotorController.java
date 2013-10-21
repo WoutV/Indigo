b89d37fb6414eb071f55bb2465785bb4a2c8e5fe
WoutV <wout.vekemans@student.kuleuven.be>
2013-10-21 16:28:42 +0200

    motorcontrole

--- IndigoZeppelin/src/zeppelin/MotorController.java
+++ IndigoZeppelin/src/zeppelin/MotorController.java
@@ -6,9 +6,15 @@ import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.RaspiPin;
 
-
+/**
+ * Controller voor de drie motoren.
+ * !!!! left: motor 1 in doc (24,4)
+ * 		right: motor 2 in doc (17,23)	
+ * 		up : motor 3 in doc (9,7)
+ * Usage: getInstance(), daarna: init met een GpioController
+ */
 public class MotorController {
-	private Motor forward;
+	private Motor up;
 	private Motor left;
 	private Motor right;
 	
@@ -21,23 +27,32 @@ public class MotorController {
 	private static MotorController mc = new MotorController();
 	
 	private MotorController() {
-		//init Motors
 	}
 	
 	/**
 	 * Geeft de enige instantie van deze singleton klasse terug
-	 * Motors zijn hierin al geinitialiseerd.
+	 * Motors zijn hierin nog niet geinitialiseerd
+	 * Hierna moet init() worden opgeroepen
 	 */
 	public static MotorController getInstance() {
 		return mc;
 	}
 	
 	/**
-	 * Zet de gpioController van de MotorController
+	 * Initialiseert de motoren.
 	 * Mag maar een keer opgeroepen worden.
+	 * GpioController moet worden meegegeven (nodig voor de motoren)
 	 */
-	public void setGpio(GpioController gpio) {
-		if(gpiocontroller == null)
+	public void init(GpioController gpio) {
+		if(gpiocontroller == null) {
 			gpiocontroller = gpio;
+
+			//init Motors
+			left = new Motor(leftfw,leftrv,gpiocontroller,Propellor.LEFT);
+			right = new Motor(rightfw,rightrv,gpiocontroller,Propellor.RIGHT);
+			up = new Motor(upfw,uprv,gpiocontroller,Propellor.UP);
+		}
 	}
+	
+	
 }
