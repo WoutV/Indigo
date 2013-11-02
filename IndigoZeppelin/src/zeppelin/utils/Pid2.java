a6d519fdd893887dccddb40f9a3a608fb423f56a
magicrune <dimitri.jonckers@student.kuleuven.be>
2013-11-02 17:54:59 +0100

    Basic PID and Pid Simulator
    added a basid Pid algorithm, a more advanced Pid algorithm, and a
    Simulator for Pid
    haven't toyed much with the Kp, Ki, Kd values yet, but Ki=Kd=0 seems to
    work

---
+++ IndigoZeppelin/src/zeppelin/utils/Pid2.java
@@ -0,0 +1,51 @@
+package zeppelin.utils;
+
+/**
+ * A class representing a PID algorithm with adjusted power output.
+ * Output is now at most 1024.
+ * Determines output based on input and how far this lies from a specified destination.
+ * Kp, Ki and Kd are constants determining the output.
+ *
+ */
+public class Pid2 extends Pid {
+	//when a larger error ==> automatically sets output to max (1024)
+	//for some values of Kp this is unnecessary
+	private double minErrorForMaxPwm = 1;
+	
+	//a similar strategy could be used for negative errors
+	
+	/**
+	 * Creates a new PID Algorithm.
+	 * @param Kp
+	 * @param Ki
+	 * @param Kd
+	 * @param dest
+	 * 		The destination
+	 * @param dt
+	 * 		The sampling frequency in ms
+	 */
+	public Pid2(double Kp, double Ki, double Kd, double dest, int dt){
+		super(Kp,Ki,Kd,dest,dt);
+	}
+	
+	/**
+	 * Determines output based on current input.
+	 * @param input
+	 * @return
+	 */
+	public double getOutput(double input) {
+		double error = dest - input;
+		integral = integral + error*dt;
+		double derivative = (error - previous_error)/dt;
+		double output = Kp*error + Ki*integral + Kd*derivative;
+		output = output + zweefpwm;
+		if(error > minErrorForMaxPwm)
+			return 1024;
+		if(output > 1024)
+			return 1024;
+		if(output < -1024)
+			return -1024;
+		return output;
+	}
+
+}
---
+++ IndigoZeppelin/src/zeppelin/utils/PidSim.java
@@ -0,0 +1,91 @@
+package zeppelin.utils;
+
+/**
+ * A class simulating the PID algorithm.
+ */
+public class PidSim {
+	
+	//Kp = 1000, zweefpwm = 400, Kd = Ki = 0;
+	//gedaan voor dt = 200 (OK), dt = 500 (minder), dt = 1 (niet goed)
+	
+	public PidSim() {
+	}
+	
+	public void run() {
+		//sampling frequency
+		int dt = 200;
+		//desired altitude
+		double dest = 2;
+		
+		//set the Kp, Kd and Ki here
+		Pid pid = new Pid(1000,0,0,dest,dt);
+		
+		//current altitude
+		double height = 0.5;
+		
+		//tolerance: close enough to destination to quit
+		double tolerance = 0.001;
+		
+		//nothing to change from here
+		double previousheight = height;
+		double v = (height-previousheight)/dt;
+		double previousv = v;
+		double a = (v - previousv)/dt;
+		double error = dest-height;
+		while(Math.abs(error) > tolerance) {
+			double output = pid.getOutput(height);
+			/*if(output > 1024)
+				output = 1024;*/
+			try {
+				Thread.sleep(dt);
+			} catch (InterruptedException e) {
+			}
+			previousheight = height;
+			height = nieuwehoogte(output,dt,v,a,height);
+			error = dest-height;
+			previousv = v;
+			v = (height-previousheight)/dt;
+			
+			a = (v - previousv)/dt;
+			System.out.println("Height: " + height + ",error: " + error + ",v: " + v + ",a: " + a + "Output: " + output);
+		}
+	}
+	
+	//bv max output: 1024 --> 5 m/s² + 9.81 --> 14.81 m/s²
+	//1024 --> max
+	//400 (= zweef) --> 9.81
+	//400/x = 9.81 ==> X (factor) = 400/9.81
+	
+	private double X = 400/9.81;
+	
+	
+	/**
+	 * Bepaalt de nieuwe hoogte op basis van de output van de motor, het interval (in ms), de snelheid, 
+	 * de vorige totale a en de huidige hoogte
+	 * @param outputmotor
+	 * @param t
+	 * @param v
+	 * @param previousa
+	 * @param height
+	 * @return
+	 */
+	public double nieuwehoogte(double outputmotor, int t, double v, double previousa, double height) {
+		//er van uit gegaan dat de outputversnelling omhoog afh van de outputpower van de motor, 
+		//lineair volgens factor X
+		//gedempt door de vorige a, waar we wel 9.81 moeten bijtellen omdat het over de a omhoog gaat
+		//nadien de echte a door er g van af te trekken
+		double a = (outputmotor/X + previousa + 9.81)/2;
+		double atotal = a - 9.81;
+		//tijd in s
+		double ts = t/1000.0;
+		double newHeight = height + v*ts + atotal*ts*ts/2;
+		return newHeight;
+	}
+	
+	public static void main(String[] args) {
+		PidSim pidsim = new PidSim();
+		pidsim.run();
+	}
+	
+	
+}
