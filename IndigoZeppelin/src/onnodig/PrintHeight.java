556807673916afaa75b8e977a4c3d7cff815935d
magicrune <dimitri.jonckers@student.kuleuven.be>
2013-11-10 21:22:53 +0100

    onnodige klasses?

---
+++ IndigoZeppelin/src/onnodig/PrintHeight.java
@@ -0,0 +1,17 @@
+package onnodig;
+
+import zeppelin.DistanceSensor;
+
+public class PrintHeight {
+	public static void main(String args[]) {
+		DistanceSensor sensor = new DistanceSensor();
+		while(true) {
+			int i=0;
+			double sum=0;
+			while(i<10){
+				sum += sensor.getHeight();
+			}
+				System.out.println(sum/10);
+		}
+	}
+}
