553c564c0246e8b0a18fd66551a376c9920f1d25
suniltandan <sunil_tandan@hotmail.com>
2014-02-13 11:15:11 +0100

    servertest

---
+++ 2desemzeppp/src/communication/ServerTest.java
@@ -0,0 +1,25 @@
+package communication;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+public class ServerTest {
+	public static class Send {
+
+		  private final static String QUEUE_NAME = "hello";
+
+		  public static void main(String[] args)
+		      throws java.io.IOException {
+			  	ConnectionFactory factory = new ConnectionFactory();
+			    factory.setHost("localhost");
+			    Connection connection = factory.newConnection();
+			    Channel channel = connection.createChannel();
+			    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+			    String message = "we shall rule!";
+			    channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+			    System.out.println(" [x] Sent '" + message + "'");
+			    channel.close();
+			    connection.close();
+		  }
+		}
+
+}
