98e3a8c38a90259d59b6d84f3127cfcf7a62dc60
qqsmalleindigoqq <wa_waxd@msn.com>
2014-03-08 14:35:24 +0100

    fotos works

--- 2desemzeppp/src/imageProcessing/ImageProcessorTester.java
+++ 2desemzeppp/src/imageProcessing/ImageProcessorTester.java
@@ -13,7 +13,7 @@ public class ImageProcessorTester {
 	 */
 	public static void main(String[] args) {
 			System.loadLibrary("opencv_java248");
-			ArrayList<ColorSymbol> arrayOfSymbols = ImageProcessor.processImage(new ImageIcon("C:/Users/Study/Desktop/OpenCv/11.jpg"));
+			ArrayList<ColorSymbol> arrayOfSymbols = ImageProcessor.processImage(new ImageIcon("./fotos/11.jpg"));
 			for(ColorSymbol cs: arrayOfSymbols){
 				System.out.println("Color:"+cs.colour);
 			}
