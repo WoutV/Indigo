ccb6702ac8c7de35a03c5d249bd6a1da8daaa753
suniltandan <sunil_tandan@hotmail.com>
2014-03-11 11:09:46 +0100

    camera ss

--- 2desemzeppp/src/camera/Camera.java
+++ 2desemzeppp/src/camera/Camera.java
@@ -2,6 +2,8 @@ package camera;
 
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -28,7 +30,14 @@ public abstract class Camera {
 	public static ImageIcon getImage(){
 		BufferedImage image = null;
 		try {
-			Process p = Runtime.getRuntime().exec("raspistill -t 10 -n -h 800 -w 800 -o image.jpg");
+			System.out.println("Give the image height:");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			int height= Integer.parseInt(br.readLine());
+			System.out.println("Give the  image width:");
+			int width= Integer.parseInt(br.readLine());
+			System.out.println("Give the  wait time:");
+			int time= Integer.parseInt(br.readLine());
+			Process p = Runtime.getRuntime().exec("raspistill -t "+time+" -n -h "+height+" -w "+width+" -o image.jpg");
 			image = ImageIO.read(p.getInputStream());
 			p.waitFor();
 		} catch (Exception e) {
