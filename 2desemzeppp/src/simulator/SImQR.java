ec6c691256604aad5a18a8cd93ff4aca7f162f76
suniltandan <sunil_tandan@hotmail.com>
2014-05-05 13:19:05 +0200

    simqr

--- 2desemzeppp/src/simulator/SImQR.java
+++ 2desemzeppp/src/simulator/SImQR.java
@@ -27,77 +27,113 @@ import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 public class SImQR {
 
-	public static String decodeQR(int tablet){
+	static Boolean encryptedLocked = false;
+
+	public static String decodeQR(int tablet) {
 		try {
+			if (encryptedLocked) {
+				synchronized (encryptedLocked) {
+					encryptedLocked.wait();
+				}
+			}
+			encryptedLocked=true;
 			String encryptedString = readQRCode(tablet);
-		} catch (NotFoundException e) {
+			write("C:\\Users\\Study\\Documents\\GitHub\\Indigo\\2desemzeppp\\keys\\encrypted",
+					encryptedString);
+			Process p = Runtime
+					.getRuntime()
+					.exec("cmd /c C:\\Python27\\python C:\\Users\\Study\\Documents\\GitHub\\Indigo\\2desemzeppp\\resources\\decription.py");
+			p.waitFor();
+			String output = read("C:\\Users\\Study\\Documents\\GitHub\\Indigo\\2desemzeppp\\keys\\result");
+			System.out.println(output);
+			encryptedLocked=false;
+			encryptedLocked.notify();
+			return output;
+			
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
+			encryptedLocked.notify();
+			encryptedLocked=false;
+			return "";
+			}
+
 	}
-	
-	public static String readQRCode(int no)
-			throws IOException, NotFoundException {
-		
-		String path = "/qrcodes/tablet"+no+".png";
-		BufferedImage bi= ImageIO.read(new File(path));
-		BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(bi)));
+
+	public static String readQRCode(int no) throws IOException,
+			NotFoundException {
+
+		String path = "/qrcodes/tablet" + no + ".png";
+		BufferedImage bi = ImageIO.read(new File(path));
+		BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(
+				new BufferedImageLuminanceSource(bi)));
 		System.out.println("Loading image completed. Reading QR Code");
 		@SuppressWarnings("unchecked")
-		Result qrCodeResult = new MultiFormatReader().decode(binaryBitmap,getMap());
-		System.out.println("QR read:"+qrCodeResult.getText());
+		Result qrCodeResult = new MultiFormatReader().decode(binaryBitmap,
+				getMap());
+		System.out.println("QR read:" + qrCodeResult.getText());
 		return qrCodeResult.getText();
 	}
-	
+
 	/***
 	 * For the things that i do not understand.
+	 * 
 	 * @return
 	 */
 	@SuppressWarnings("rawtypes")
-	private static Map getMap(){
-		Map<EncodeHintType, ErrorCorrectionLevel>hintMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
+	private static Map getMap() {
+		Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
 		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
 		return hintMap;
 	}
-	
+
 	static int numberOfTries = 0;
-	private static String sendRSAKeyToTablet(){
-		try{
-		Process p =Runtime.getRuntime().exec("cmd /c C:\\Python27\\python C:\\Users\\Study\\Documents\\GitHub\\Indigo\\2desemzeppp\\resources\\keys.py");
-		p.waitFor();
-		
-		String text ="";
-		numberOfTries =0;
-		while(numberOfTries < 10){
-			try{
-			text = QRCodeReader.readQRCode();
-			break;
+
+	private static String sendRSAKeyToTablet() {
+		try {
+			if (encryptedLocked) {
+				synchronized (encryptedLocked) {
+					encryptedLocked.wait();
+				}
 			}
-			catch(IOException|NotFoundException exception){
-				numberOfTries++;
+			encryptedLocked=true;
+			Process p = Runtime
+					.getRuntime()
+					.exec("cmd /c C:\\Python27\\python C:\\Users\\Study\\Documents\\GitHub\\Indigo\\2desemzeppp\\resources\\keys.py");
+			p.waitFor();
+
+			String text = "";
+			numberOfTries = 0;
+			while (numberOfTries < 10) {
+				try {
+					text = QRCodeReader.readQRCode();
+					break;
+				} catch (IOException | NotFoundException exception) {
+					numberOfTries++;
+				}
 			}
-		}
-		write("C:\\Users\\Study\\Documents\\GitHub\\Indigo\\2desemzeppp\\keys\\encrypted", text);
-		p = Runtime.getRuntime().exec("cmd /c C:\\Python27\\python C:\\Users\\Study\\Documents\\GitHub\\Indigo\\2desemzeppp\\resources\\decription.py");
-		p.waitFor();
-		String output = read("C:\\Users\\Study\\Documents\\GitHub\\Indigo\\2desemzeppp\\keys\\result");
-		System.out.println(output);
-		return output;
-		}catch(Exception e){
-			//doe niks
+			write("C:\\Users\\Study\\Documents\\GitHub\\Indigo\\2desemzeppp\\keys\\encrypted",
+					text);
+			p = Runtime
+					.getRuntime()
+					.exec("cmd /c C:\\Python27\\python C:\\Users\\Study\\Documents\\GitHub\\Indigo\\2desemzeppp\\resources\\decription.py");
+			p.waitFor();
+			String output = read("C:\\Users\\Study\\Documents\\GitHub\\Indigo\\2desemzeppp\\keys\\result");
+			System.out.println(output);
+			encryptedLocked=false;
+			encryptedLocked.notify();
+			return output;
+			
+		} catch (Exception e) {
+			// doe niks
+			encryptedLocked=false;
+			encryptedLocked.notify();
 		}
 		return "Oops";
-	
-		
+
 	}
-	
-	
-	public static String read(String fileName){
+
+	public static String read(String fileName) {
 		String result = "";
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(fileName));
@@ -111,12 +147,12 @@ public class SImQR {
 			}
 			result = sb.toString();
 			br.close();
-		} catch(IOException e) {
+		} catch (IOException e) {
 		}
 		return result;
 	}
-	
-	public static void write(String fileName, String text){
+
+	public static void write(String fileName, String text) {
 		PrintWriter writer;
 		try {
 			writer = new PrintWriter(fileName, "UTF-8");
